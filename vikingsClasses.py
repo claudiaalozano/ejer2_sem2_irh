0604a181d44519f874405a7581d63eda8485e82c
claudiaalozano <clau.lozano.perez@gmail.com>
2022-10-24 12:55:22 +0200

    creo las clases

---
+++ vikingsClasses.py
@@ -0,0 +1,19 @@
+#Soldados
+class Soldier:
+    def __init__(self,health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+    
+    def reciveDamage(self, damage):
+        self.health -=  damage
+
+
+class Vikings(Soldier):
+    def __init__(self, name, health, strength):
+        Soldier.__init__(self,health, strength)
+        self.name = name
+    
+    def att
