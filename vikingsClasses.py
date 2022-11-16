7b5a63e6e31faacb6c0676c4af2ee2f1d590fb67
claudiaalozano <clau.lozano.perez@gmail.com>
2022-11-16 08:55:30 +0100

    creo las clases para vikingos y saxon

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -16,4 +16,32 @@ class Vikings(Soldier):
         Soldier.__init__(self,health, strength)
         self.name = name
     
+    def attack(self):
+        return super().attack()
+    
+    def reciveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return "{} has recived {} points of damage".format(self.name, damage)
+        else:
+            return "{} has died in the combat".format(self.name)
+        
+    def battleCry(self):
+        return "Odin Owns You ALL!"
+
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        Soldier.__init__(self, health, strength)
+    
+    def attack(self):
+        return super().attack()
+    
+    def reciveDamage(self, damage):
+        self.health -=  damage
+        if self.health > 0:
+            return "A Saxon has received {} points of damage".format(damage)
+        else: 
+            return"A Saxon has died in the combat"
+
+class War:
     
